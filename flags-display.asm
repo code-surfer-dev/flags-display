50453ea5e3360193278c3bf78edfbe986c6e7065
Ingo Schmied <ingo@codesurfer.dev>
2022-09-14 09:03:17 -0400

    bugfixes - Set IM 1, start A at 1 instead of 0

--- flags-display.asm
+++ flags-display.asm
@@ -8,31 +8,39 @@ B_DAT		.EQ $01		;PIO PORT B data
 A_CTL		.EQ $02		;PIO PORT A control
 B_CTL   	.EQ $03		;PIO PORT B control
 
-    EI                  ;Enable interrupts
-    CALL RESTART
+    JP RESTART
 
-    .OR $0008           ;Use RST08 for OUTPUT subroutine
-OUTPUT:
+    .OR $0038           ;Use RST 56 for OUTPUT subroutine, The
+                        ;default handler for Mode 1 Interrupts
+OUTPUT:                 
     DI                  ;Disable interrupts
     PUSH BC             ;Push BC on the stack
     PUSH AF             ;Push Flags on the stack
     POP BC              ;Load B with A, & C with Flags
     LD B, C             ;Load Flags into B
     OUT (A_DAT), A      ;Send A to PORT A
-    LD C, B_DAT         ;
-    OUT (C), E          ;Send F via E to PORT B
+    LD C, B_DAT         ;Set C to PORT B address
+    OUT (C), B          ;Send F via B to PORT B
     POP BC              ;Restore BC
     EI                  ;Enable interrupts
     RETI
 
     .OR $0100           ;Make room for other RST handlers
 RESTART:
+    LD SP, $FFFF        ;Initialize the stack pointer
     LD A, $0F           ;Set PIO PORT A to output
     OUT (A_CTL), A
     LD A, $0F           ;Set PIO PORT B to output
     OUT (B_CTL), A
-    LD A, $08           ;Set PIO interrupt vector
+    IM 1
+    EI                  ;Enable interrupts
 
-    LD A, $00           ;Start at $00
+    ;JP USER_PROGRAM    ;Could jump to a program in RAM  
 
+    LD A, $01           ;Simple test in ROM to start with
+LOOP:
+
+    INC A
+    JR NZ,LOOP
+    HALT
     
